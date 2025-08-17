c01dad0486fa1851fc993c3fe79a52f2a53066ef
soike lab <sukiluvcode@icloud.com>
2025-08-17 22:54:11 +0800

    commmit

---
+++ contexualized_extracton.py
@@ -0,0 +1,8 @@
+# Implementation of contexualized extraction: Frist extract properties separately then link it with correponds synthesis routes via description of that material.
+
+
+# suppose paragraphs are correctly labeled
+
+# Define extraction agent for each property, here we test for (ys, uts, strain) & phase
+
+
